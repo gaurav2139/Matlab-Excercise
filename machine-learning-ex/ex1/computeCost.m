870ea76b7d077184e05da2d2856002530d2ab33c
Gaurav <38208095+gaurav2139@users.noreply.github.com>
2020-03-31 17:29:38 +0530

    Update computeCost.m

--- machine-learning-ex/ex1/computeCost.m
+++ machine-learning-ex/ex1/computeCost.m
@@ -13,8 +13,8 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
-
-
+h_x = X*theta;    
+J = sum((h_x - y) .^ 2)/2/m; 
 
 
 % =========================================================================
